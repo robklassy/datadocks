4d0a31bf2c540ed93f93b5b88327babe6e6d4c8b
Rob K <kalsikam@gmail.com>
2024-09-06 19:59:35 -0400

    update specs

--- spec/requests/reservations_spec.rb
+++ spec/requests/reservations_spec.rb
@@ -8,9 +8,38 @@ RSpec.describe 'reservations', type: :request do
   end
 
   path '/reservations' do
-
     get('list reservations') do
       response(200, 'successful') do
+
+        example 'application/json', :reservation, [
+          {
+            "id"=>"32a0b18a-98a2-4610-a284-c6a79d35f7d4",
+            "customer_id"=>"f835547b-a0eb-462e-823e-f7688cdecfce",
+            "table_id"=>"1e78b073-608f-4662-813d-430b807998a3",
+            "restaurant_id"=>"252d7e7f-b794-4b7a-9639-4f6ff1a3974d",
+            "start_at"=>1080,
+            "end_at"=>1140,
+            "number_of_guests"=>2,
+            "state"=>"assigned",
+            "date_of_reservation"=>"2024-09-07T23:14:01.654Z",
+            "created_at"=>"2024-09-06T23:14:01.667Z",
+            "updated_at"=>"2024-09-06T23:14:01.667Z"
+          },
+
+          {
+            "id"=>"32a0b18a-98a2-4610-a284-c6a79d35f7d4",
+            "customer_id"=>"f835547b-a0ab-462e-823e-f7688cdecf12",
+            "table_id"=>"1e78b073-608f-4662-813d-430b807998a3",
+            "restaurant_id"=>"252d7e7f-b794-4b7a-9639-4f6ff1a3974d",
+            "start_at"=>980,
+            "end_at"=>1040,
+            "number_of_guests"=>4,
+            "state"=>"assigned",
+            "date_of_reservation"=>"2024-09-07T23:14:01.654Z",
+            "created_at"=>"2024-09-06T23:14:01.667Z",
+            "updated_at"=>"2024-09-06T23:14:01.667Z"
+          }
+        ]
 
         after do |example|
           example.metadata[:response][:content] = {
@@ -27,7 +56,54 @@ RSpec.describe 'reservations', type: :request do
     end
 
     post('create reservation') do
+      consumes 'application/json'
+      produces 'application/json'
+
+      parameter name: :reservation, in: :body, schema: {
+        type: :object,
+        properties: {
+          customer_id: { type: :string },
+          restaurant_id: { type: :string },
+          start_at: { type: :string },
+          end_at: { type: :string },
+          number_of_guests: { type: :string },
+          date_of_reservation: { type: :string },
+          optimized: { type: :string }
+        }
+      }
+
       response(200, 'successful') do
+        example 'application/json', :reservation, {
+          "id"=>"32a0b18a-98a2-4610-a284-c6a79d35f7d4",
+          "customer_id"=>"f835547b-a0eb-462e-823e-f7688cdecfce",
+          "table_id"=>"1e78b073-608f-4662-813d-430b807998a3",
+          "restaurant_id"=>"252d7e7f-b794-4b7a-9639-4f6ff1a3974d",
+          "start_at"=>1080,
+          "end_at"=>1140,
+          "number_of_guests"=>2,
+          "state"=>"assigned",
+          "date_of_reservation"=>"2024-09-07T23:14:01.654Z",
+          "created_at"=>"2024-09-06T23:14:01.667Z",
+          "updated_at"=>"2024-09-06T23:14:01.667Z"
+        }
+
+        let(:reservation) do
+          c = Customer.all.sample
+          r = Restaurant.all.sample
+          {
+            customer_id: c.id,
+            restaurant_id: r.id,
+            date_of_reservation: Time.zone.now + 1.days,
+            number_of_guests: 2,
+            start_at: 18 * 60,
+            end_at: 19 * 60,
+            optimized: false
+          }
+        end
+
+        let(:optimized) do
+          { optimized: 'false' }
+        end
 
         after do |example|
           example.metadata[:response][:content] = {
@@ -36,7 +112,73 @@ RSpec.describe 'reservations', type: :request do
             }
           }
         end
-        run_test!
+        run_test! do |response|
+          data = JSON.parse(response.body)
+          expect(data['table_id']).not_to eq(nil)
+        end
+      end
+    end
+
+    post('create reservation optimized') do
+      consumes 'application/json'
+      produces 'application/json'
+
+      parameter name: :reservation, in: :body, schema: {
+        type: :object,
+        properties: {
+          customer_id: { type: :string },
+          restaurant_id: { type: :string },
+          start_at: { type: :string },
+          end_at: { type: :string },
+          number_of_guests: { type: :string },
+          date_of_reservation: {type: :string },
+          optimized: { type: :string }
+        }
+      }
+
+      response(200, 'successful') do
+        example 'application/json', :reservation, {
+          "id"=>"32a0b18a-98a2-4610-a284-c6a79d35f7d4",
+          "customer_id"=>"f835547b-a0eb-462e-823e-f7688cdecfce",
+          "table_id"=>"1e78b073-608f-4662-813d-430b807998a3",
+          "restaurant_id"=>"252d7e7f-b794-4b7a-9639-4f6ff1a3974d",
+          "start_at"=>1080,
+          "end_at"=>1140,
+          "number_of_guests"=>2,
+          "state"=>"assigned",
+          "date_of_reservation"=>"2024-09-07T23:14:01.654Z",
+          "created_at"=>"2024-09-06T23:14:01.667Z",
+          "updated_at"=>"2024-09-06T23:14:01.667Z"
+        }
+
+        let(:reservation) do
+          c = Customer.all.sample
+          r = Restaurant.all.first
+          seats = r.tables.pluck(:number_of_seats).sample
+          ids = ServerTable.where(table_id: r.tables.pluck(:id))
+
+          {
+            customer_id: c.id,
+            restaurant_id: r.id,
+            date_of_reservation: Time.zone.now,
+            number_of_guests: seats,
+            start_at: 13 * 60,
+            end_at: 14 * 60,
+            optimized: 'true'
+          }
+        end
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test! do |response|
+          data = JSON.parse(response.body)
+          expect(data['table_id']).not_to eq(nil)
+        end
       end
     end
   end
@@ -46,7 +188,21 @@ RSpec.describe 'reservations', type: :request do
 
     get('show reservation') do
       response(200, 'successful') do
-        let(:id) { Reservation }
+        example 'application/json', :reservation, {
+        "id"=>"32a0b18a-98a2-4610-a284-c6a79d35f7d4",
+        "customer_id"=>"f835547b-a0eb-462e-823e-f7688cdecfce",
+        "table_id"=>"1e78b073-608f-4662-813d-430b807998a3",
+        "restaurant_id"=>"252d7e7f-b794-4b7a-9639-4f6ff1a3974d",
+        "start_at"=>1080,
+        "end_at"=>1140,
+        "number_of_guests"=>2,
+        "state"=>"assigned",
+        "date_of_reservation"=>"2024-09-07T23:14:01.654Z",
+        "created_at"=>"2024-09-06T23:14:01.667Z",
+        "updated_at"=>"2024-09-06T23:14:01.667Z"
+      }
+
+        let(:id) { Reservation.last.id }
 
         after do |example|
           example.metadata[:response][:content] = {
@@ -55,18 +211,50 @@ RSpec.describe 'reservations', type: :request do
             }
           }
         end
-        run_test!
+        run_test! do |response|
+          data = JSON.parse(response.body)
+          expect(data['id']).to eq(id)
+        end
       end
     end
   end
 
   path '/restaurants/{restaurant_id}/reservations' do
-    # You'll want to customize the parameter types...
     parameter name: 'restaurant_id', in: :path, type: :string, description: 'restaurant_id'
 
     get('list reservations') do
       response(200, 'successful') do
-        let(:restaurant_id) { '123' }
+
+        example 'application/json', :reservation, [
+          {
+            "id"=>"32a0b18a-98a2-4610-a284-c6a79d35f7d4",
+            "customer_id"=>"f835547b-a0eb-462e-823e-f7688cdecfce",
+            "table_id"=>"1e78b073-608f-4662-813d-430b807998a3",
+            "restaurant_id"=>"252d7e7f-b794-4b7a-9639-4f6ff1a3974d",
+            "start_at"=>1080,
+            "end_at"=>1140,
+            "number_of_guests"=>2,
+            "state"=>"assigned",
+            "date_of_reservation"=>"2024-09-07T23:14:01.654Z",
+            "created_at"=>"2024-09-06T23:14:01.667Z",
+            "updated_at"=>"2024-09-06T23:14:01.667Z"
+          },
+
+          {
+            "id"=>"32a0b18a-98a2-4610-a284-c6a79d35f7d4",
+            "customer_id"=>"f835547b-a0ab-462e-823e-f7688cdecf12",
+            "table_id"=>"1e78b073-608f-4662-813d-430b807998a3",
+            "restaurant_id"=>"252d7e7f-b794-4b7a-9639-4f6ff1a3974d",
+            "start_at"=>980,
+            "end_at"=>1040,
+            "number_of_guests"=>4,
+            "state"=>"assigned",
+            "date_of_reservation"=>"2024-09-07T23:14:01.654Z",
+            "created_at"=>"2024-09-06T23:14:01.667Z",
+            "updated_at"=>"2024-09-06T23:14:01.667Z"
+          }
+        ]
+        let(:restaurant_id) { Restaurant.last.id }
 
         after do |example|
           example.metadata[:response][:content] = {
@@ -75,7 +263,10 @@ RSpec.describe 'reservations', type: :request do
             }
           }
         end
-        run_test!
+        run_test! do |response|
+          data = JSON.parse(response.body)
+          expect(data.count).to eq(1)
+        end
       end
     end
   end
--- spec/requests/tables_spec.rb
+++ spec/requests/tables_spec.rb
@@ -4,6 +4,7 @@ Rails.application.load_tasks
 
 RSpec.describe 'tables', type: :request do
   before(:all) do
+    Reservation.delete_all
     Rake::Task["db:seed"].invoke
   end
 
@@ -65,10 +66,62 @@ RSpec.describe 'tables', type: :request do
     end
   end
 
-  path '/tables/occupied' do
+  path '/tables/occupied?date_of_reservation={date_of_reservation}&start_at={start_at}&end_at={end_at}' do
+    parameter name: 'date_of_reservation', in: :path, type: :string, description: 'date_of_reservation'
+    parameter name: 'start_at', in: :path, type: :string, description: 'start_at'
+    parameter name: 'end_at', in: :path, type: :string, description: 'end_at'
 
     get('occupied tables') do
       response(200, 'successful') do
+
+        example 'application/json', :discussion_question_post, [
+            {"id"=>"f2780449-2af1-4611-848f-606e560b9de7",
+            "identifier"=>"20",
+            "number_of_seats"=>6,
+            "active"=>true,
+            "restaurant_id"=>"fabdd375-03ed-47bd-8b68-3d730a6298b0",
+            "created_at"=>"2024-09-06T20:24:08.166Z",
+            "updated_at"=>"2024-09-06T20:24:08.166Z"},
+          {"id"=>"605fb246-46ed-4afc-98fb-323f7785d423",
+            "identifier"=>"25",
+            "number_of_seats"=>2,
+            "active"=>true,
+            "restaurant_id"=>"252d7e7f-b794-4b7a-9639-4f6ff1a3974d",
+            "created_at"=>"2024-09-06T20:24:08.183Z",
+            "updated_at"=>"2024-09-06T20:24:08.183Z"},
+          {"id"=>"a7dcc970-b615-4f91-8f64-16d0f86a0691",
+            "identifier"=>"31",
+            "number_of_seats"=>8,
+            "active"=>true,
+            "restaurant_id"=>"53c89f01-92f9-46b0-8df8-128143fc5e10",
+            "created_at"=>"2024-09-06T20:24:08.202Z",
+            "updated_at"=>"2024-09-06T20:24:08.202Z"},
+          {"id"=>"254c1896-9611-4c95-b8b9-b75ffb320387",
+            "identifier"=>"34",
+            "number_of_seats"=>6,
+            "active"=>true,
+            "restaurant_id"=>"c8eb4cd2-99a1-4b02-8230-9ff6038f0cff",
+            "created_at"=>"2024-09-06T20:24:08.211Z",
+            "updated_at"=>"2024-09-06T20:24:08.211Z"},
+          {"id"=>"ee065406-d273-4af9-90f7-eaefca65c943",
+            "identifier"=>"37",
+            "number_of_seats"=>6,
+            "active"=>true,
+            "restaurant_id"=>"68e7d183-ec0d-4884-98b0-3a36a8ca4b19",
+            "created_at"=>"2024-09-06T20:24:08.220Z",
+            "updated_at"=>"2024-09-06T20:24:08.220Z"},
+          {"id"=>"1da1c4ff-a9d8-47f0-a81e-1585d046e509",
+            "identifier"=>"48",
+            "number_of_seats"=>6,
+            "active"=>true,
+            "restaurant_id"=>"eaa183f0-3c3a-4028-8328-c6a01a4b530e",
+            "created_at"=>"2024-09-06T20:24:08.261Z",
+            "updated_at"=>"2024-09-06T20:24:08.261Z"}
+        ]
+
+        let(:date_of_reservation) { Time.zone.now }
+        let(:start_at) { 12 * 60 }
+        let(:end_at) { 20 * 60 }
 
         after do |example|
           example.metadata[:response][:content] = {
@@ -78,6 +131,8 @@ RSpec.describe 'tables', type: :request do
           }
         end
         run_test! do |response|
+          data = JSON.parse(response.body)
+          pp data
         end
       end
     end
